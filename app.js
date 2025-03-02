b5c9ee943a0379b67f9df34a103328e983228166
Ishan Saini <ishansaini0105@gmail.com>
2025-03-02 10:41:05 +0530

    Chat page UI and socket io on both client and server side is setup

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express')
 const indexRouter= require('./routes/index.js')
 const path = require('path')
 const app = express()
+const http= require('http')
+const socketio = require('socket.io')
+
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection',(socket)=>{
+    console.log('connected');
+})
 
 app.set('view engine', 'ejs')
 app.use(express.json())
@@ -11,4 +20,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter)
 
-app.listen(3000)
+server.listen(3000)
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,10 @@ res.render('index.ejs')
 
 })
 
+Router.get('/chat', (req,res)=>{
+
+    res.render('chat.ejs')
+    
+    })
+
 module.exports = Router
